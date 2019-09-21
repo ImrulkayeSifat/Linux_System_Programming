3536d1bed2a88799c07c5793ce318e8159d3795b
Imrul Kayes Sifat <41978816+ImrulkayeSifat@users.noreply.github.com>
2019-09-21 15:46:56 -0400

    Add files via upload

---
+++ signal_handler.c
@@ -0,0 +1,20 @@
+#include<signal.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+void sig_handler(int signum){
+    printf("Alarm delivered signum is %d\n",signum);
+    system("/bin/ls");
+    //exit(0);
+}
+
+int main()
+{
+    alarm(5);
+    signal(SIGALRM,sig_handler);
+
+    for(;;){
+        sleep(1);
+    }
+    return 0;
+}
